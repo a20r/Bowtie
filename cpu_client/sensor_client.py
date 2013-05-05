0ce4273fe3310904ebacbd6076dbce5708e37132
aw204 <aw204@st-andrews.ac.uk>
2013-05-05 03:09:55 +0100

    Change the way the the parameters are entered on the CPU client side

--- cpu_client/sensor_client.py
+++ cpu_client/sensor_client.py
@@ -13,18 +13,26 @@ __author__ = "Alex Wallar <aw204@st-andrews.ac.uk>"
 
 class SensorDataGetter:
 
-	def __init__(self, http_ser, droid_id):
+	def __init__(self, http_ser, port, droid_id):
+		"""
+		For the droid_id I would like to use the MAC address of the 
+		CPU client.
+		"""
 		self.droid_id = droid_id
 		self.http_ser = http_ser
+		self.port = port
+
+	def get_data_url(self):
+		return self.http_ser + ':' + str(self.port) + '/json_data/' + self.droid_id + '.json'
 
 	def get_data(self):
-		ser = urllib2.urlopen(self.http_ser + '/' + self.droid_id + '.json')
+		ser = urllib2.urlopen(self.get_data_url())
 		s_data = ser.read()
 		ser.close()
 		return json.loads(s_data)
 
 if __name__ == '__main__':
-	gsd = SensorDataGetter('http://127.0.0.1:5000/json_data', 'sensor_data')
+	gsd = SensorDataGetter('http://127.0.0.1', 5000, 'sensor_data')
 	while True:
 		print gsd.get_data()
 		time.sleep(0.5)
