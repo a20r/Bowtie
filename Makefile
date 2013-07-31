a8d118cd0f63c76cf73b7588a5aa36dcce4d92d1
Chris Choi <chutsu@gmail.com>
2013-07-31 13:59:08 +0100

    Add ability to obtain video stream from webcam
    Can now obtain the video stream from the webcam via HTML5. The code also
    chops up the stream into a series of jpegs ready to transmit to the
    server, however that part is still in development.

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 all:
-	echo 'Enter run_server or run_cpu_client'
+	@echo 'options:'
+	@echo "\t run_server"
+	@echo "\t run_cpu_client"
+	@echo ''
 
 dependency_check:
 	@python DEPENDENCIES; if [ $$? -eq 1 ]; then exit -1; fi;
 
 run_server:
-	cd bowtie/server; go run httpgo.go; cd ../..
+	@echo "Running Bowtie server ..."
+	cd bowtie/server; go run bowtie_server.go; cd ../..
 
 run_client: dependency_check
+	@echo "Running Bowtie client ..."
 	cd bowtie/cpu_client; python sensor_client.py; cd ../..
--- bowtie/server/bowtie_server.go
+++ bowtie/server/bowtie_server.go
@@ -1,6 +1,5 @@
 package main
 
-// Imports
 import (
     "fmt"
     "io/ioutil"
@@ -36,13 +35,15 @@ func (r Response) String() (s string) {
 }
 
 // Opens a file and returns it represented
-// as a Page. 
+// as a Page.
 func loadPage(folder, title string) (*Page, error) {
     filename := folder + "/" + title
     body, err := ioutil.ReadFile(filename)
+
     if err != nil {
         return nil, err
     }
+
     return &Page{Title: title, Body: body}, nil
 }
 
@@ -50,21 +51,24 @@ func loadPage(folder, title string) (*Page, error) {
 // for static and template responses. See Usage!
 func fileResponseCreator(folder string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-                fmt.Println("GET\t" + r.URL.Path)
-    			var p *Page
-    			var err error
-    			if len(r.URL.Path) == 1 {
-                    // In case the path is just '/'
-    				p, err = loadPage("templates", "index.html")
-    			} else {
-    				p, err = loadPage(folder, r.URL.Path[1:])
-    			}
-    			if p != nil {
-        			w.Write(p.Body)
-        		} else {
-        			fmt.Println("ERROR\t" + err.Error())
-        		}
-		}
+        var p *Page
+        var err error
+
+        fmt.Println("GET\t" + r.URL.Path)
+
+        if len(r.URL.Path) == 1 {
+            // In case the path is just '/'
+            p, err = loadPage("templates", "index.html")
+        } else {
+            p, err = loadPage(folder, r.URL.Path[1:])
+        }
+
+        if p != nil {
+            w.Write(p.Body)
+        } else {
+            fmt.Println("ERROR\t" + err.Error())
+        }
+    }
 }
 
 // Removes the JSON data once the node stops
@@ -74,6 +78,7 @@ func dataRemoveHandler(w http.ResponseWriter, r *http.Request) {
     urlVars := strings.Split(r.URL.Path[1:], "/")
     cpu_id, node_id := urlVars[1], urlVars[2]
     err := os.Remove("json_data/" + cpu_id + "/" + node_id + ".json")
+
     if err != nil {
         fmt.Println("ERROR\t" + err.Error())
     }
@@ -82,41 +87,53 @@ func dataRemoveHandler(w http.ResponseWriter, r *http.Request) {
 // Handler called when data is sent
 // to the server from a node
 func dataSentHandler(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
     fmt.Println("POST\t" + r.URL.Path)
+
+    // Parse form and extract data details
+    r.ParseForm()
     urlVars := strings.Split(r.URL.Path[1:], "/")
-    cpu_id, node_id := urlVars[1], urlVars[2]
+    cpu_id := urlVars[1]
+    node_id := urlVars[2]
     path := "./json_data/" + cpu_id + "/"
+
+    // Make and log data to a file
     os.Mkdir(path, os.ModePerm | os.ModeType)
     file, err := os.Create(path + node_id + ".json")
+
+    // Error handler
     if err != nil {
         fmt.Println("ERROR\t" + err.Error())
         return
     }
+
     file.Write([]byte(r.Form["sensor_data"][0]))
     file.Close()
 }
 
 // Responds to the GET request from a client.
-// Used for the visualization and for APIs 
+// Used for the visualization and for APIs
 // for users to query the data.
 func dataGetHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println("GET\t" + r.URL.Path)
     urlVars := strings.Split(r.URL.Path[1:], "/")
     cpu_id := urlVars[1]
     files, err := ioutil.ReadDir("json_data/" + cpu_id)
+
     if err != nil {
         res := Response{"error": Response{"code": 2, "message": "No data for " + cpu_id}}
         fmt.Fprint(w, res)
         fmt.Println("ERROR\t" + err.Error())
+
     } else {
         res := make(Response)
+
         for _, file := range files {
             //fmt.Println(file.Name())
             var sData SensorData
             node_id := strings.Split(file.Name(), ".")[0]
             file_bytes, read_err := ioutil.ReadFile("json_data/" + cpu_id + "/" + node_id + ".json")
             json_err := json.Unmarshal(file_bytes, &sData)
+
             if read_err != nil {
                 fmt.Println("ERROR\t" + read_err.Error())
             }
@@ -125,20 +142,21 @@ func dataGetHandler(w http.ResponseWriter, r *http.Request) {
             }
             res[node_id] = sData
         }
+
         //fmt.Println(files)
         if len(files) > 0 {
             res["error"] = Response{"code": 0, "message": "No error"}
         } else {
-            res["error"] = Response{"code": 2, "message": "No data for " + cpu_id} 
+            res["error"] = Response{"code": 2, "message": "No data for " + cpu_id}
         }
+
         fmt.Println("RESPONSE\t" + res.String())
         fmt.Fprint(w, res)
     }
 }
 
-// Handles all Javascript, images, and HTML
-// file requests
-func displayHandler() {
+// Handles all incomming http requests
+func requestHandler() {
     staticHandler := fileResponseCreator("static")
     http.HandleFunc("/", fileResponseCreator("templates"))
     http.HandleFunc("/css/", staticHandler)
@@ -147,14 +165,19 @@ func displayHandler() {
     http.HandleFunc("/favicon.ico", fileResponseCreator("static/img"))
 }
 
+// MAIN EXECUTION FLOW
 func main() {
-    displayHandler()
+    requestHandler()
+
     http.HandleFunc("/checked/", dataSentHandler)
     http.HandleFunc("/unchecked/", dataRemoveHandler)
     http.HandleFunc("/get_data/", dataGetHandler)
+
     var addr_flag = flag.String("addr", "localhost", "Address the http server binds to")
     var port_flag = flag.String("port", "8080", "Port used for http server")
+
     flag.Parse()
+
     //fmt.Println("Running server on " + *addr_flag + ":" + *port_flag)
     http.ListenAndServe(*addr_flag + ":" + *port_flag, nil)
 }
